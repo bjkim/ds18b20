2484a311dafa7e6cd53fc124b9234dc0fe3a93ce
byungjung <byungjung.kim@gmail.com>
2013-06-18 11:40:06 +0900

    add id and value on result

--- samples/index.js
+++ samples/index.js
@@ -6,17 +6,16 @@
 
 var sensor = require('..');
 
-sensor.sensors(function(err, ids) {
+sensor.sensors(function (err, ids) {
   if (err) {
     console.log('Can not get sensor IDs', err);
   } else {
-    console.log(ids);
     for (var id in ids) {
-      sensor.temperature(ids[id], function(err, result) {
+      sensor.temperature(ids[id], function (err, result) {
         if (err) {
           console.log('Can not get temperature from sensor', err);
         } else {
-          console.log('Sensor ' + ids[id] + ' :', result);
+          console.log('[' + result.id + ']', result.value);
         }
       });
     }
